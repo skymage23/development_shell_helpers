76f801682723f982c2c2daa22709e3cb267ed78d
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-24 13:15:37 -0600

    Added "Universal/repo-utils" for POSIX shell and PowerShell. Added "Write-RepoCorruptMessage" API for all "Universal/repo-utils" files.

--- imports/PowerShell/Universal/repo-utils.ps1
+++ imports/PowerShell/Universal/repo-utils.ps1
@@ -1,7 +1,14 @@
+if($ImportsPowerShellUniversalRepoUtilsPs1){
+    return
+}
+
+$ImportsPowerShellUniversalRepoUtilsPs1 = $true
+
 function Write-RepoCorruptMessage {
     Write-Host @"
 Something happened to your copy of the code repository.
 A file or directory that is required is missing.  You
 will have to reset the repository to a previous commit.
+
 "@ 
 }
--- imports/posix_shell/Universal/repo-utils.sh
+++ imports/posix_shell/Universal/repo-utils.sh
@@ -1,4 +1,9 @@
-function Write-RepoCorruptMessage {
+if [ "$ImportsPowerShellUniversalRepoUtilsPs1" != "" ]; then
+    return
+fi
+
+export ImportsPowerShellUniversalRepoUtilsPs1=1
+function Write-RepoCorruptMessage() {
     echo "Something happened to your copy of the code repository.
 A file or directory that is required is missing.  You
 will have to reset the repository to a previous commit.
