0bde74742c284764358b9b3601a978102ab062bd
Christopher Loyd Nugent <skymage23.cn@gmail.com>
2025-02-24 21:12:41 -0600

    Added PowerShell-specific helper function "Use-InThisBlockOnly".

---
+++ imports/PowerShell/Specific/ps-utils.psm1
@@ -0,0 +1,30 @@
+if($ImportsPowerShellSpecificPsUtilsPsm1){
+    return
+}
+$ImportsPowerShellSpecificPsUtilsPsm1 = $true
+
+#Serves the same purpose as a "using" block
+#in C# or a "with" statement in Python.
+#This works, but if you want to give the 
+#object a specific name, you MUST
+#use a "param()" statement inside your
+#script block.
+#
+#The "IDisposable" object will always be passed as the only
+#parameter to the provided script block.
+#
+function Use-InThisBlockOnly{
+    param(
+        [Parameter(Mandatory=$true)]
+        [IDisposable] $object,
+        [Parameter(Mandatory=$true)]
+        [ScriptBlock] $block
+    )
+
+    try {
+        $null = &$block $object
+    } finally {
+        $object.Dispose()
+    }
+}
+Export-ModuleMember -Function Use-InThisBlockOnly
